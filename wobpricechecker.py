0a556ad2304cf0489e2b697892b5290a4e95680e
David <davidmillett@protonmail.com>
2025-10-15 16:31:56 +0100

    used selenium to get price

--- wobpricechecker.py
+++ wobpricechecker.py
@@ -1,10 +1,27 @@
-from urllib.request import urlopen
-import re
+# from urllib.request import urlopen
+# import re
 
 url = 'https://www.worldofbooks.com/en-gb/products/sword-art-online-9-book-reki-kawahara-9780316390422'
 
-def extract_data(url):
-    page = urlopen(url)
-    print(page)
+# def extract_data(url):
+#     page = urlopen(url)
+#     html_bytes = page.read()
+#     html = html_bytes.decode("utf-8")
+#     start_index = html.find('<span class="price_item')
+#     end_index = html.find('<div id="rrp-container')
+#     page_body = html[start_index:end_index]
+#     print(page_body)
 
-extract_data(url)
+# extract_data(url)
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+price = driver.find_element(By.CLASS_NAME, "price-item").text
+print(price)
+
+driver.quit()
