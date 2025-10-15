0e69799f19c554b952d9caef5dc2c8be5757bc52
David <davidmillett@protonmail.com>
2025-10-15 17:45:59 +0100

    added list of books to check sequentially

--- wobpricechecker.py
+++ wobpricechecker.py
@@ -2,18 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-
+# Book class
 class Book:
     def __init__(self, name, url, price):
         self.name = name
         self.url = url
         self.price = price
 
-SAO9 = Book("SAO9", 'https://www.worldofbooks.com/en-gb/products/sword-art-online-9-book-reki-kawahara-9780316390422', 0)
+# Books
+books = [
+    Book("SAO9", 'https://www.worldofbooks.com/en-gb/products/sword-art-online-9-book-reki-kawahara-9780316390422', 0),
+    Book("SAO11", 'https://www.worldofbooks.com/en-gb/products/sword-art-online-11-book-reki-kawahara-9780316390446', 0),
+]
 
+# Setup Chrome
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
+driver.implicitly_wait(0.5)
+
+# Function to fetch price
 def get_price(book):
     driver.get(book.url)
     try:
@@ -21,6 +29,9 @@ def get_price(book):
     except:
         book.price = "Price not found"
     print(f'{book.name} Price: {book.price}')
-    driver.quit()
 
-get_price(SAO9)
+# Run
+for book in books:
+    get_price(book)
+
+driver.quit()
