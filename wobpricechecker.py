d1e2c4893c48b54fd289a5bcd136925f14eeffde
David <davidmillett@protonmail.com>
2025-10-15 17:25:12 +0100

    remade into formula to allow use for different books

--- wobpricechecker.py
+++ wobpricechecker.py
@@ -1,5 +1,10 @@
-# from urllib.request import urlopen
-# import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
 
 class Book:
     def __init__(self, name, url, price):
@@ -9,32 +14,13 @@ class Book:
 
 SAO9 = Book("SAO9", 'https://www.worldofbooks.com/en-gb/products/sword-art-online-9-book-reki-kawahara-9780316390422', 0)
 
-# def extract_data(url):
-#     page = urlopen(url)
-#     html_bytes = page.read()
-#     html = html_bytes.decode("utf-8")
-#     start_index = html.find('<span class="price_item')
-#     end_index = html.find('<div id="rrp-container')
-#     page_body = html[start_index:end_index]
-#     print(page_body)
+def get_price(book):
+    driver.get(book.url)
+    try:
+        book.price = driver.find_element(By.CLASS_NAME, "price-item").text
+    except:
+        book.price = "Price not found"
+    print(f'{book.name} Price: {book.price}')
+    driver.quit()
 
-# extract_data(url)
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-
-driver.get(SAO9.url)
-
-try:
-    SAO9.price = driver.find_element(By.CLASS_NAME, "price-item").text
-except:
-    SAO9.price = "Price not found"
-
-print(f'{SAO9.name} Price: {SAO9.price}')
-
-driver.quit()
+get_price(SAO9)
